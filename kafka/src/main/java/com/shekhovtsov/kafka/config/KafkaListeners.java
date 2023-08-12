addef36c9bd56877dbac3d8d569985d466a4895b
shekhovtsovsergey <89629417575@mail.ru>
2023-08-12 21:51:17 +0300

    dev

--- kafka/src/main/java/com/shekhovtsov/kafka/config/KafkaListeners.java
+++ kafka/src/main/java/com/shekhovtsov/kafka/config/KafkaListeners.java
@@ -1,7 +1,20 @@
 package com.shekhovtsov.kafka.config;
 
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KafkaListeners {
 
+
+    @KafkaListener(
+            topics = "test",
+            groupId = "test"
+    )
+    void listner (String data){
+        System.out.println("listner received:" + data);
+    }
 
 
 }
---
+++ kafka/src/main/java/com/shekhovtsov/kafka/controller/MessageRequest.java
@@ -0,0 +1,4 @@
+package com.shekhovtsov.kafka.controller;
+
+public record MessageRequest (String message){
+}
