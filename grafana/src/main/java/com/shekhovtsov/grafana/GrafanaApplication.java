381fc0b56dee615a7e1067e626fc558acfb720f3
shekhovtsovsergey <89629417575@mail.ru>
2024-02-15 21:19:41 +0300

    project

---
+++ grafana/src/main/java/com/shekhovtsov/grafana/GrafanaApplication.java
@@ -0,0 +1,16 @@
+package com.shekhovtsov.grafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrafanaApplication.class, args);
+    }
+
+}
+
+
+//test
